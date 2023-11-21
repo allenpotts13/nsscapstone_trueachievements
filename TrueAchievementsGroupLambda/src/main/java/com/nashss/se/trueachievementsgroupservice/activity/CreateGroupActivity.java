ce200a73887165f983a82396a06ce1571d01c794
Allen_Potts <allenpotts13@gmail.com>
2023-11-21 11:55:06 -0600

    CreateGroupLambda, Activity, Request, Response all implemented. Also a test for the Activity class was written.

--- TrueAchievementsGroupLambda/src/main/java/com/nashss/se/trueachievementsgroupservice/activity/CreateGroupActivity.java
+++ TrueAchievementsGroupLambda/src/main/java/com/nashss/se/trueachievementsgroupservice/activity/CreateGroupActivity.java
@@ -1,10 +1,20 @@
 package com.nashss.se.trueachievementsgroupservice.activity;
 
+import com.nashss.se.trueachievementsgroupservice.activity.requests.CreateGroupRequest;
+import com.nashss.se.trueachievementsgroupservice.activity.results.CreateGroupResult;
+import com.nashss.se.trueachievementsgroupservice.converters.ModelConverter;
 import com.nashss.se.trueachievementsgroupservice.dynamodb.GroupDao;
 
+import com.nashss.se.trueachievementsgroupservice.dynamodb.models.Game;
+import com.nashss.se.trueachievementsgroupservice.dynamodb.models.Group;
+import com.nashss.se.trueachievementsgroupservice.exceptions.InvalidAttributeException;
+import com.nashss.se.trueachievementsgroupservice.models.GroupModel;
+import com.nashss.se.trueachievementsgroupservice.utils.TrueAchievementGroupServiceUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.inject.Inject;
 
 /**
@@ -25,5 +35,50 @@ public class CreateGroupActivity {
     public CreateGroupActivity(GroupDao groupDao) {
         this.groupDao = groupDao;
 
+    }
+
+    /**
+     * This method handles the incoming request by persisting a new group
+     * with the provided group name and user ID from the request.
+     * <p>
+     * It then returns the newly created group.
+     * <p>
+     * If the provided group name or user ID has invalid characters, throws an
+     * InvalidAttributeValueException
+     *
+     * @param createGroupRequest request object containing the group name and user ID
+     *                              associated with it
+     * @return createGroupResult result object containing the API defined {@link GroupModel}
+     */
+
+    public CreateGroupResult handleRequest(final CreateGroupRequest createGroupRequest) {
+        logger.info("Received CreateGroupRequest {}", createGroupRequest);
+
+        if (!TrueAchievementGroupServiceUtils.isValidString(createGroupRequest.getGroupName())) {
+            throw new InvalidAttributeException("Group name [" + createGroupRequest.getGroupName() +
+                "] contains illegal characters");
+        }
+
+        if (!TrueAchievementGroupServiceUtils.isValidString(createGroupRequest.getUserId())) {
+            throw new InvalidAttributeException("User ID [" + createGroupRequest.getUserId() +
+                "] contains illegal characters");
+        }
+
+        Set<Game> gamesList = null;
+        if (createGroupRequest.getGamesList() != null) {
+            gamesList = new HashSet<>(createGroupRequest.getGamesList());
+        }
+
+        final Group group = new Group();
+        group.setGroupName(createGroupRequest.getGroupName());
+        group.setUserId(createGroupRequest.getUserId());
+        group.setGamesList(gamesList);
+
+        groupDao.saveGroup(group);
+
+        GroupModel groupModel = new ModelConverter().toGroupModel(group);
+        return CreateGroupResult.builder()
+            .withGroup(groupModel)
+            .build();
     }
 }
--- TrueAchievementsGroupLambda/src/main/java/com/nashss/se/trueachievementsgroupservice/activity/results/CreateGroupResult.java
+++ TrueAchievementsGroupLambda/src/main/java/com/nashss/se/trueachievementsgroupservice/activity/results/CreateGroupResult.java
@@ -1,4 +1,47 @@
 package com.nashss.se.trueachievementsgroupservice.activity.results;
 
+import com.nashss.se.trueachievementsgroupservice.models.GroupModel;
+
 public class CreateGroupResult {
+
+    private final GroupModel group;
+
+    /**
+     * Implementation of the CreateGroupResults for the TrueAchievements Group Service CreateGroup API.
+     *
+     * @param group the {@link GroupModel} is how a group is formatted.
+     */
+
+    public CreateGroupResult(GroupModel group) {
+        this.group = group;
+    }
+
+    public GroupModel getGroup() {
+        return group;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateGroupResult{" +
+                "group=" + group +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private GroupModel group;
+
+        public Builder withGroup(GroupModel group) {
+            this.group = group;
+            return this;
+        }
+
+        public CreateGroupResult build() {
+            return new CreateGroupResult(group);
+        }
+    }
 }
