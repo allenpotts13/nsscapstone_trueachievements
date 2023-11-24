4cc90b1746fe7cefa8b3eb1ccc064ad05e181e33
Allen_Potts <allenpotts13@gmail.com>
2023-11-23 18:23:56 -0600

    GetAllGroupsLambda, Activity, Request, Response all implemented. Working on tests still.

--- TrueAchievementsGroupLambda/src/main/java/com/nashss/se/trueachievementsgroupservice/activity/GetAllGroupsActivity.java
+++ TrueAchievementsGroupLambda/src/main/java/com/nashss/se/trueachievementsgroupservice/activity/GetAllGroupsActivity.java
@@ -1,7 +1,15 @@
 package com.nashss.se.trueachievementsgroupservice.activity;
 
+import com.nashss.se.trueachievementsgroupservice.activity.requests.GetAllGroupsRequest;
+import com.nashss.se.trueachievementsgroupservice.activity.results.GetAllGroupsResult;
+import com.nashss.se.trueachievementsgroupservice.converters.ModelConverter;
 import com.nashss.se.trueachievementsgroupservice.dynamodb.GroupDao;
+import com.nashss.se.trueachievementsgroupservice.dynamodb.models.Group;
+import com.nashss.se.trueachievementsgroupservice.models.GroupModel;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javax.inject.Inject;
 
 /**
@@ -20,5 +28,27 @@ public class GetAllGroupsActivity {
     @Inject
     public GetAllGroupsActivity(GroupDao groupDao) {
         this.groupDao = groupDao;
+    }
+
+    /**
+     * This method handles the incoming request by retrieving all existing groups.
+     * <p>
+     * It then returns the groups.
+     *
+     * @param getAllGroupsRequest request object containing the user ID associated with it
+     * @return getAllGroupsResult result object containing the API defined GroupModel
+     */
+
+    public GetAllGroupsResult handleRequest(final GetAllGroupsRequest getAllGroupsRequest) {
+        ModelConverter mc = new ModelConverter();
+        List<Group> groupList = groupDao.getAllGroups(getAllGroupsRequest.getUserId());
+        List<GroupModel> groupModelList = new ArrayList<>();
+        for (Group group : groupList) {
+            groupModelList.add(mc.toGroupModel(group));
+        }
+        Collections.sort(groupModelList);
+        return GetAllGroupsResult.builder()
+            .withGroupList(groupModelList)
+            .build();
     }
 }
